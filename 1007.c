32de0481bdc50fc288bd631e70592743f38f1016
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2015-04-19 04:27:48 -0300

    URI Online Judge | 1007
    URI Online Judge | 1007
    Diferença
    Adaptado por Neilor Tonin, URI  Brasil
    Timelimit: 1
    Linguagem C

---
+++ 1007.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 0 , b = 0, c = 0, d = 0,  DIFERENCA = 0;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    DIFERENCA = ( (a*b) - (c*d) ) ;
+    printf("DIFERENCA = %d\n",DIFERENCA);
+
+    return 0;
+}
